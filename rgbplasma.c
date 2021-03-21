8464acdef6d647e90b8927cdd5c19935020b3c7b
Mika Tuupola <tuupola@appelsiini.net>
2021-03-21 04:19:48 +0200

    Avoid floats when possible

--- rgbplasma.c
+++ rgbplasma.c
@@ -38,11 +38,11 @@ void rgbplasma_render()
 {
     for (uint16_t x = 0; x < DISPLAY_WIDTH; x++) {
         for (uint16_t y = 0; y < DISPLAY_HEIGHT; y++) {
-                float v1 = 128.0 + (128.0 * sin((x + step) / 32.0));
-                float v2 = 128.0 + (128.0 * sin((y + step) / 24.0));
-                float v3 = 128.0 + (128.0 * sin(sqrt((x * x) + y * y) / 12.0));
-                float v = (v1 + v2 + v3) / 3;
-                color_t color = hagl_color(v, 256 - v, 128);
+                uint8_t v1 = 128 + (128 * sin((x + step) / 32.0));
+                uint8_t v2 = 128 + (128 * sin((y + step) / 24.0));
+                uint8_t v3 = 128 + (128 * sin(sqrt((x * x) + y * y) / 12.0));
+                uint8_t v = (v1 + v2 + v3) / 3;
+                color_t color = hagl_color(v, 255 - v, 128);
                 hagl_put_pixel(x, y, color);
         }
     }
